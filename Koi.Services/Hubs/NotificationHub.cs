e04b2a214e47bff060905c8ce947aadfab140dde
Le Quoc Uy <lequocuyit@gmail.com>
2024-09-10 19:27:01 +0700

    setup signalR

---
+++ Koi.Services/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Koi.Services.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendToAUser(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
