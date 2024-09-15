73460cefe621b0d3aac88ff5ede211d1e4a149ac
Le Quoc Uy <lequocuyit@gmail.com>
2024-09-16 00:40:54 +0700

    change to camelcase

--- Koi.BusinessObjects/FAQ.cs
+++ Koi.BusinessObjects/FAQ.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Koi.BusinessObjects
+﻿namespace Koi.BusinessObjects
 {
     public class FAQ : BaseEntity
     {
         public string Question { get; set; }
         public string Answer { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
     }
 }
