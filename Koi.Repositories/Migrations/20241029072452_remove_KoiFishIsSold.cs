6a3783aa2cdc80293ee9428df866ed92a262503e
Nguyễn Vũ <115633834+KanaeRudo00@users.noreply.github.com>
2024-10-29 14:41:44 +0700

    fix available for sale, remove IsSold KoiFish

---
+++ Koi.Repositories/Migrations/20241029072452_remove_KoiFishIsSold.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Koi.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_KoiFishIsSold : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSold",
+                table: "KoiFishs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSold",
+                table: "KoiFishs",
+                type: "bit",
+                nullable: true);
+        }
+    }
+}
