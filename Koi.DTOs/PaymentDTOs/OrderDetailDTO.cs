f5b4870db5e92f7b81ccd947d62c3815313c2b8a
Hoang Tien <minhtien11052003@gmail.com>
2024-10-30 14:01:03 +0700

    fix mapping

--- Koi.DTOs/PaymentDTOs/OrderDetailDTO.cs
+++ Koi.DTOs/PaymentDTOs/OrderDetailDTO.cs
@@ -11,13 +11,16 @@ namespace Koi.DTOs.PaymentDTOs
     public class OrderDetailDTO
     {
         public int Id { get; set; }
+        public int OrderId { get; set; }
         public int KoiFishId { get; set; }
+
         public int? ConsignmentForNurtureId { get; set; }
 
         public long Price { get; set; }
         public string? Status { get; set; }
         public string? ShippingStatus { get; set; }
         public string? NurtureStatus { get; set; }
+        public int? StaffId { get; set; }
         public virtual KoiFishResponseDTO KoiFish { get; set; }
         public virtual ConsignmentForNurtureDTO? ConsignmentForNurture { get; set; }
     }
