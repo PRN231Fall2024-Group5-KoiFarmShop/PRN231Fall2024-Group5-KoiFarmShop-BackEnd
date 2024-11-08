03c16a978fd20e18a1d39f3803f59f726f1231a5
Hoang Tien <minhtien11052003@gmail.com>
2024-11-08 19:00:58 +0700

    patch 3 complete

--- Koi.DTOs/ConsignmentDTOs/ConsignmentRequestDTO.cs
+++ Koi.DTOs/ConsignmentDTOs/ConsignmentRequestDTO.cs
@@ -10,7 +10,7 @@ namespace Koi.DTOs.ConsignmentDTOs
     {
         public int KoiFishId { get; set; }
         public int DietId { get; set; } // Foreign Key to Diet
-        public DateTime StartDate { get; set; }  // Ngày bắt đầu chăm sóc
+        public DateTime? StartDate { get; set; }  // Ngày bắt đầu chăm sóc
         public DateTime EndDate { get; set; }  // Ngày kết thúc chăm sóc
         public string? Note { get; set; }
     }
